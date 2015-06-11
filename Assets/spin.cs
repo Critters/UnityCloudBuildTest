06652167d0600d510776fea0771189bdcc53885e
Critters <dave@enbiggen.com>
2015-06-10 17:26:33 -0700

    Initial commit

---
+++ Assets/spin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spin : MonoBehaviour {
+	void Start () {
+	
+	}
+	
+	void Update () {
+		transform.Rotate(new Vector3(0.5f, 1.8f, 1f), Time.deltaTime*180f);
+	}
+}
